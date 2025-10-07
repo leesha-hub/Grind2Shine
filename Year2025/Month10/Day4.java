98178329e76cb5d8985326e3bc862c1335bb2a80
leesha-hub <leesha1223@naver.com>
2025-10-07 12:40:37 +0900

    20251004 Programmers 완전범죄 ver2

--- Year2025/Month10/Day4.java
+++ Year2025/Month10/Day4.java
@@ -7,21 +7,28 @@ public class Day4 {
     public static void main(String[] args) {
         Solution4 ss = new Solution4();
 
-//        int[][] info = {{1, 2}, {2, 3}, {2, 1}};
-//        int n = 4;
-//        int m = 4;
-
         int[][] info = {{1, 2}, {2, 3}, {2, 1}};
-        int n = 1;
-        int m = 7;
+        int n = 4;
+        int m = 4;
+//        result : 2
+
+//        int[][] info = {{1, 2}, {2, 3}, {2, 1}};
+//        int n = 1;
+//        int m = 7;
+
+//        result : 0
 //
 //        int[][] info = {{3,3}, {3,3}};
 //        int n = 7;
 //        int m = 1;
+
+//        result : 6
 //
 //        int[][] info = {{3,3}, {3,3}};
 //        int n = 6;
 //        int m = 1;
+
+//        result : -1
 
         ss.solution(info, n, m);
     }
@@ -37,19 +44,26 @@ class Solution4 {
         int traceAMax = n;
         int traceBMax = m;
         int traceA = 0;
+        int traceB = 0;
+        int traceBcnt = 0;
         // 출력 확인
         for (int[] row : info) {
             System.out.println(Arrays.toString(row));
+            traceB += row[1];
             if (traceBMax - row[1] <= 0) {
-                traceA += row[0];
-            } else {
-                traceBMax -= row[1];
+                traceBcnt++;
+            }
+            traceBMax -= row[1];
+        }
+        if (traceB >= traceBMax) {
+            for (int i = 0; i < traceBcnt; i++) {
+                traceA += info[i][0];
             }
         }
-        if(traceA > 0 && traceA < traceAMax) {
+        if (traceA > 0 && traceA < traceAMax) {
             answer = traceA;
-        } else if(traceA == 0) {
-            answer = traceA;
+        } else if (traceA == 0) {
+            answer = 0;
         } else {
             answer = -1;
         }
