7d437b2de5d4de834beb8ea18df3484cd024f9a3
leesha-hub <leesha1223@naver.com>
2025-10-10 14:55:58 +0900

    20251010 Programmers 서버 증설 횟수

---
+++ Year2025/Month10/Day10.java
@@ -0,0 +1,25 @@
+package Year2025.Month10;
+import java.util.*;
+
+public class Day10 {
+    public static void main(String[] args) {
+        Solution10 ss = new Solution10();
+
+        int[] players = {0, 2, 3, 3, 1, 2, 0, 0, 0, 0, 4, 2, 0, 6, 0, 4, 2, 13, 3, 5, 10, 0, 1, 5};
+        int m = 3;
+        int k = 5;
+
+        ss.solution(players, m, k);
+    }
+}
+
+class Solution10 {
+    public int solution(int[] players, int m, int k) {
+        int answer = 0;
+
+        m = 3;
+        k = 5;
+
+        return answer;
+    }
+}
