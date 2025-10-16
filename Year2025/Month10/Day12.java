b42bbea38adf70f301a04a1e605f6431dfe77b84
leesha-hub <leesha1223@naver.com>
2025-10-16 21:52:49 +0900

    20251012 Programmers 지게차와 크레인

--- Year2025/Month10/Day12.java
+++ Year2025/Month10/Day12.java
@@ -3,9 +3,21 @@ package Year2025.Month10;
 public class Day12 {
     public static void main(String[] args) {
         Solution12 ss = new Solution12();
+//
+//        storage	requests	result
+//        ["AZWQY", "CAABX", "BBDDA", "ACACA"]	["A", "BB", "A"]	11
+//        ["HAH", "HBH", "HHH", "HAH", "HBH"]	["C", "B", "B", "B", "B", "H"]	4
+        String[] storage = {"AZWQY", "CAABX", "BBDDA", "ACACA"};
+        String[] requests = {"A", "BB", "A"};
+//        String[] storage = {"HAH", "HBH", "HHH", "HAH", "HBH"};
+//        String[] requests = {"C", "B", "B", "B", "B", "H"};
+        ss.solution(storage, requests);
     }
 }
 
 class Solution12 {
-
+    public int solution(String[] storage, String[] requests) {
+        int answer = 0;
+        return answer;
+    }
 }
