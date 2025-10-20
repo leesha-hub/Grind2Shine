4d9e5d1d09befd3f9f87c7b1a37d7dafa6f120a1
leesha-hub <leesha1223@naver.com>
2025-10-20 21:49:17 +0900

    20251020 Programmers 부모의 형질을 모두 가지는 대장균 찾기

--- Year2025/Month10/Day17.sql
+++ Year2025/Month10/Day17.sql
@@ -1,27 +1,29 @@
 SELECT BIN(GENOTYPE) AS binary_value FROM ECOLI_DATA;
 
 WITH RECURSIVE ecoliData AS (
-    SELECT ID FROM ECOLI_DATA
+    SELECT ID, GENOTYPE FROM ECOLI_DATA -- where id = 8
 ),
 seq AS (
-    SELECT ID, BIN(ID) AS bin_str, LENGTH(BIN(ID)) AS len, 1 AS pos
+    SELECT ID, BIN(GENOTYPE) AS bin_str, LENGTH(BIN(GENOTYPE)) AS len, 1 AS pos
       FROM ecoliData
      UNION ALL
     SELECT ID, bin_str, len, pos + 1
       FROM seq
      WHERE pos < len
 )
+# select * from seq;
 SELECT
-    ed.ID,
-    ed.GENOTYPE as gt,
-    ed.PARENT_ID as pg,
-    GENOTYPE,
+    ed.ID as ed_id
+    ed.GENOTYPE as ed_gt,
+    (select genotype from ECOLI_DATA  where id = ed.parent_id) as parent_genotype,
     bin_str,
     len,
     pos,
-    len - pos + 1 AS bit_position
+    len - pos + 1 AS bit_position,
+    ed.parent_id
   FROM seq
-  JOIN ECOLI_DATA ed
-    ON seq.ID = ed.PARENT_ID
+  LEFT JOIN ECOLI_DATA ed
+    ON seq.ID = ed.ID
  WHERE SUBSTRING(bin_str, pos, 1) = '1'
- ORDER BY ed.id
+  -- AND ed.parent_id IS NOT NULL
+ ORDER BY id, bin_str
